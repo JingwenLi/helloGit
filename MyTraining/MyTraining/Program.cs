2d032e01534c12b70a40a0d1a63de929a361f9c1
JingwenLi <571392690@qq.com>
2019-09-15 17:11:07 +0800

    ·

--- MyTraining/MyTraining/Program.cs
+++ MyTraining/MyTraining/Program.cs
@@ -22,25 +22,6 @@ namespace MyLeetCode
 
         public static void Main()
         {
-
-            using (Imap imap = new Imap())
-            {
-                imap.Connect("imap.server.com");
-                imap.UseBestLogin("lijingwen831@163.com", "ljw831wy");
-                imap.SelectInbox();
-                List<long> uids = imap.Search(Flag.Unseen);
-                foreach (long uid in uids)
-                {
-                    IMail email = new MailBuilder()
-                        .CreateFromEml(imap.GetMessageByUID(uid));
-                    Console.WriteLine(email.Subject);
-                }
-                imap.Close();
-            }
-
-
-
-            Console.WriteLine(A.b);
             Console.ReadLine();
         }
     }
