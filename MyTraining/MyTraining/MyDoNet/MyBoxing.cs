2a30acae8a890d35bcc3f81f78d11b69a8900df1
JingwenLi <571392690@qq.com>
2019-09-29 23:05:55 +0800

    1

--- MyTraining/MyTraining/MyDoNet/MyBoxing.cs
+++ MyTraining/MyTraining/MyDoNet/MyBoxing.cs
@@ -7,19 +7,17 @@ namespace MyTraining.MyDoNet
     {
         public static void Run()
         {
-            int a = 1;
-            object b = (int)a;        //装箱  创建了a的一个新的值, b是对新值的引用, 因此修改a, b不会发生变化
+            //装箱和拆箱会降低性能，增大操作开销，创建众多的对象，加重垃圾回收器的负担
+
+            int a = 1; 
+            object b = (int)a;    //装箱  创建了a的一个新的值, b是对新值的引用, 因此修改a, b不会发生变化
 
             a = 2;
             Debug.WriteLine(b);
-
-
-            int c = (int)b;     //拆箱  同理 也是创建了一个副本
+            
+            int c = (int)b;       //拆箱  同理 也是创建了一个副本
             b = 2;
             Debug.WriteLine(c);
-
-
-            //装箱和拆箱会降低性能，增大操作开销，创建众多的对象，加重垃圾回收器的负担
         }
     }
 
--- MyTraining/MyTraining/MyDoNet/MyOutRef.cs
+++ MyTraining/MyTraining/MyDoNet/MyOutRef.cs
@@ -1,78 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyTraining.MyDoNet
 {
+    /// <summary>
+    /// https://blog.csdn.net/u012664198/article/details/82837874
+    /// </summary>
+
+
+    class MyClassC
+    {
+        public int Val = 20;
+    }
+
+    /*  *  *  *  *  *  *  *  *  *   ref 与 out  *  *  *  *  *  *  *  *  *  *  *  *  *  
+     * ref在外部必须赋值 (报错：使用了未赋值的局部变量）
+     * out在外部可以不赋值, 但是到了内部就需要赋值了 (报错：使用了未赋值的out参数）
+     * ref和out很相近，对于out为什么要求在方法内赋值，主要是区别于ref。
+     * 引入out主要是为了开发人员明确被out标记的参数主要用于传出一个值，而不是传入并传出一个值。
+     */
     class MyOutRef
     {
-        public void OutMethod(out int a, out string b)
-        {
-            //以下语句会报错：使用了未赋值的out参数
-            //a += 2;
-
-            //以下语句正常
-            a = 2;
-            a += 2;
-
-            //out在外部可以不赋值, 但是到了内部就需要赋值了,不赋值无法使用,编译器会报错
-            //ref和out很相近，对于out为什么要求在方法内赋值，主要是区别于ref。
-            //引入out主要是为了开发人员明确被out标记的参数主要用于传出一个值，而不是传入并传出一个值。
-            b = "bb";
-
-            Debug.WriteLine("a:{0},b:{1}", a, b);   //a: 4,b: bb
-        }
-
-
-        public void RefMethod(ref int a, ref string b)
-        {
-            a += 2;
-            b += "bb";
-
-            Debug.WriteLine("a:{0},b:{1}", a, b);   //a: 3,b: bbb
-        }
-
-        public void Method(int a, string b)
-        {
-            a += 2;
-            b += "bb";
-
-            Debug.WriteLine("a:{0},b:{1}", a, b);  //a: 3,b: bbb
-        }
-
         public void Run()
         {
-            #region out
-            //out在外部赋不赋值均可
             int a = 1;
             string b = "b";
-            OutMethod(out a, out b);
-            Debug.WriteLine("a:{0},b:{1}", a, b);   //a: 4,b: bb
-            #endregion
-
+            MyClassC c = new MyClassC();
+            OutMethod(out a, out b, out c);
+            Debug.WriteLine("a:{0}, b:{1}, c:{2}", a, b, c.Val);   
             
-            #region ref
-            //ref在外部必须赋值
-            //以下语句会报错：使用了未赋值的局部变量c/d
-            //int c;
-            //string d;
-            //RefMethod(ref c, ref d);
-
-            //在方法内修改引用对象在方法外生效
             a = 1;
             b = "b";
-            RefMethod(ref a, ref b);
-            Debug.WriteLine("a:{0},b:{1}", a, b);   //a: 3,b: bbb
-            #endregion
+            c = new MyClassC();
+            RefMethod(ref a, ref b, ref c);
+            Debug.WriteLine("a:{0}, b:{1}, c:{2}", a, b, c.Val);
 
-            //在方法内修改引用对象在方法外不生效
             a = 1;
             b = "b";
-            Method(a, b);
-            Debug.WriteLine("a:{0},b:{1}", a, b);   //a:1,b:b
+            c = new MyClassC();
+            Method(a, b, c);
+            Debug.WriteLine("a:{0}, b:{1}, c:{2}", a, b, c.Val);
+        }
+
+        public void Method(int a, string b, MyClassC c)
+        {
+            a += 2;
+            b += "bb";
+            c.Val += 3;
+            Debug.WriteLine("a:{0}, b:{1}, c:{2}", a, b, c.Val);
+        }
+
+        public void OutMethod(out int a, out string b, out MyClassC c)
+        {
+            a = 2;
+            a += 2;
+            b = "bb";
+            c = new MyClassC();
+            c.Val += 3;
+            Debug.WriteLine("a:{0}, b:{1}, c:{2}", a, b, c.Val);
+        }
+
+        public void RefMethod(ref int a, ref string b, ref MyClassC c)
+        {
+            a += 2;
+            b += "bb";
+            c.Val += 3;
+            Debug.WriteLine("a:{0}, b:{1}, c:{2}", a, b, c.Val);
+        }
+    }
+
+    [TestClass]
+    public class TestMyOutRef
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            new MyOutRef().Run();
         }
     }
 }
